549d6e284cff1e1615e46057d55978bcb0ecd4e1
Grant Cummings <grantcummings4429@gmail.com>
2024-04-05 09:34:38 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    }
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    })
+        .then((res) => res.json())
+        .then((userObj) => {
+            return document.body.innerHTML = userObj.id
+        })
+        .catch((error) => {
+            alert("Error! Failed POST Request!")
+            return document.body.innerHTML = error
+          })
+}
+
